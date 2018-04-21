035d98002694a7a00a2df408739f546a01755d51
chiyo <cinro9@gmail.com>
2018-04-21 15:25:03 +0800

    firstcommit2

--- test/TestMainModule.c
+++ test/TestMainModule.c
@@ -39,12 +39,12 @@ int Test_Is_Not_Equal(int a, int b, int real)
 
 void Test1()
 {
-	CU_ASSERT(Test_Is_Equal(3, 4, 7));
+	CU_ASSERT(Test_Is_Equal(3, 4, 6));
 }
 
 void Test2()
 {
-	CU_ASSERT(Test_Is_Not_Equal(4, 5, 10));
+	CU_ASSERT(Test_Is_Not_Equal(4, 5, 8));
 }
 
 /*0 表示成功，1表示失败*/
